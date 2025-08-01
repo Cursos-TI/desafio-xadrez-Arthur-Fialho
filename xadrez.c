fe1e50ea5afbe2a71a8d68b42e97abf019029284
Arthur Fialho <161606595+Arthur-Fialho@users.noreply.github.com>
2025-08-01 21:51:56 +0000

    adicionado código nível aventureiro

--- xadrez.c
+++ xadrez.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 // Desafio de Xadrez - MateCheck
-//Nível Novato
+//Nível Aventureiro
 
 int main() {
     // Nível Novato - Movimentação das Peças
@@ -9,6 +9,8 @@ int main() {
     int bispoMov = 5;
     int torreMov = 5;
     int rainhaMov = 8;
+    int cavaloMov = 2;
+    int escolha;
 
     // Implementação de Movimentação do Bispo
     // Estrutura de repetição para simular a movimentação do Bispo em diagonal.
@@ -19,24 +21,73 @@ int main() {
     printf("\nPróxima movimentação: \n");
     // Implementação de Movimentação da Torre
     // Estrutura de repetição para simular a movimentação da Torre para a direita.
-    int i = 0;
+    int i_torre = 0;
     do{
         printf("Torre moveu para direita\n");
-        i++;
-    }while(i < torreMov);
+        i_torre++;
+    }while(i_torre < torreMov);
 
     printf("\nPróxima movimentação: \n");
     // Implementação de Movimentação da Rainha
     // Estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-    i = 0;
-    while(i < rainhaMov){
+    int i_rainha = 0;
+    while(i_rainha < rainhaMov){
         printf("Rainha moveu para esquerda\n");
-        i++;
-    };
+        i_rainha++;
+    }
 
+    printf("\nPróxima movimentação: \n");
     // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
+    // Loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
+    printf("Escolha qual direção o cavalo ira se mover em L, 1-cima-esquerda, 2-cima-direita, 3-baixo-esquerda, 4-baixo-direita\n");
+    scanf("%d", &escolha);
+
+    switch(escolha){
+        case 1:
+        for(int i = 0; i < 1; i++){
+            int j = 0;
+            while(j < cavaloMov){
+                printf("Cavalo moveu para cima\n");
+                j++;
+            }
+            printf("Cavalo moveu para esquerda\n");
+        }
+        break;
+        case 2:
+        for(int i = 0; i < 1; i++){
+            int j = 0;
+            while(j < cavaloMov){
+                printf("Cavalo moveu para cima\n");
+                j++;
+            }
+            printf("Cavalo moveu para direita\n");
+        }
+        break;
+        case 3:
+        for(int i = 0; i < 1; i++){
+            int j = 0;
+            while(j < cavaloMov){
+                printf("Cavalo moveu para baixo\n");
+                j++;
+            }
+            printf("Cavalo moveu para esquerda\n");
+        }
+        break;
+        case 4:
+        for(int i = 0; i < 1; i++){
+            int j = 0;
+            while(j < cavaloMov){
+                printf("Cavalo moveu para baixo\n");
+                j++;
+            }
+            printf("Cavalo moveu para direita\n");
+        }
+        break;
+        default:
+        printf("Escolha invalida!\n");
+        break;
+    }
 
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
